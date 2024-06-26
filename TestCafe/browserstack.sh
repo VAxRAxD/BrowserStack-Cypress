993b31ff11054b46ab145f7f259bc2f69c37efd5
VAxRAxD <varadp763@gmail.com>
2024-06-26 21:07:39 +0530

    Cypress Percy

--- TestCafe/browserstack.sh
+++ TestCafe/browserstack.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export $(grep -v '^#' .env | xargs)
-echo $BROWSERSTACK_USERNAME
-echo $BROWSERSTACK_ACCESS_KEY
+export BROWSERSTACK_USE_AUTOMATE=1
+export BROWSERSTACK_CAPABILITIES_CONFIG_PATH="./browserstack-config.json"
