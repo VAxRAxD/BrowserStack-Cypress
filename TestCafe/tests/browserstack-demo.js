44d5606127351e7f2aed5ab0c85c14c295bca17e
VAxRAxD <varadp763@gmail.com>
2024-06-28 12:44:54 +0530

    Added Testcafe Percy

--- TestCafe/tests/browserstack-demo.js
+++ TestCafe/tests/browserstack-demo.js
@@ -1,19 +1,32 @@
-import { Selector } from 'testcafe';
-fixture('TestCafe example page').page(
-  'https://devexpress.github.io/testcafe/example/'
-);
-test("Should submit developer's name successfully", async user => {
-  const interfaceSelect = Selector('#preferred-interface');
-  const interfaceOption = interfaceSelect.find('option');
-
-  await user
-    .typeText('#developer-name', 'Sourav')
-    .click('#remote-testing')
-    .click(interfaceSelect)
-    .click(interfaceOption.withText('Both'))
-    .expect(interfaceSelect.value)
-    .eql('Both')
-    .click('#submit-button')
-    .expect(Selector('#article-header').innerText)
-    .eql('Thank you, Sourav!');
+import percySnapshot from "@percy/testcafe";
+import { Selector } from "testcafe";
+fixture`bstackdemo`.page`https://www.bstackdemo.com/`;
+test("BStack Demo test", async (t) => {
+  await t
+    .click(Selector('#\\31  > div.shelf-item__buy-btn').withText("Add to cart"))
+    .click(Selector(".buy-btn"))
+    .click("#username svg")
+    .click(Selector("#react-select-2-option-0-0").with({ force: true }))
+    .click("#password svg")
+    .click(Selector("#react-select-3-option-0-0").with({ force: true }))
+    .click(Selector("#login-btn"))
+    .typeText(
+      Selector("label").withText("First Name").sibling("input"),
+      "Varad"
+    )
+    .typeText(
+      Selector("label").withText("Last Name").sibling("input"),
+      "Prabhu"
+    )
+    .typeText(Selector("label").withText("Address").sibling("input"), "Dadar")
+    .typeText(
+      Selector("label").withText("State/Province").sibling("input"),
+      "Maharashtra"
+    )
+    .typeText(
+      Selector("label").withText("Postal Code").sibling("input"),
+      "400028"
+    )
+    .click(Selector("#checkout-shipping-continue"))
+  await percySnapshot(t, "Order Placed Successfully");
 });
